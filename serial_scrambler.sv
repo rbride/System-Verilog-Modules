6355a8c752a5742c25ff4ea8c6d340531901f46d
ryan <rbride@mail.bradley.edu>
2023-05-21 18:36:21 -0400

    Fixed some stuff

--- serial_scrambler.sv
+++ serial_scrambler.sv
@@ -15,19 +15,20 @@ module serial_scrambler(
     //Define Outputs and Inputs
     input wire CLK;
     input wire Bit_In;
-    input wire enable
+    input wire enable;
+    input wire reset;
     output logic Bit_Out;
     //x^1 + x^58 + x^39; S stores current state
     reg [57:0] s;
     assign feedback = { Bit_In ^ s[57]  ^ s[38]};
     assign Bit_Out = feedback;
 
-    always @(posedge clk) begin
+    always @(posedge CLK) begin
         if (reset == 1'b1) begin
             s <= 0;
         end
         else if (enable == 1'b1) begin
-            states <= { s[56:0], feedback};
+            s <= { s[56:0], feedback};
         end
     end
 endmodule
