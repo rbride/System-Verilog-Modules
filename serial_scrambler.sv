00eced44c33531a0d9e88b64d0287feec7c4eca8
ryan <rbride@mail.bradley.edu>
2023-05-21 18:13:15 -0400

    Cleaned up code

--- serial_scrambler.sv
+++ serial_scrambler.sv
@@ -24,16 +24,10 @@ module serial_scrambler(
 
     always @(posedge clk) begin
         if (reset == 1'b1) begin
-            s <= 58'b0000000000000000000000000000000000000000000000000000000000; 
+            s <= 0;
         end
         else if (enable == 1'b1) begin
-            states <= { s[57]:, s[55], s[54], s[53], s[52], s[51], s[50], s[49], s[48],
-                        s[47], s[46], s[45], s[44], s[43], s[42], s[41], s[40], s[39],
-                        s[38], s[37], s[36], s[35], s[34], s[33], s[32], s[31], s[30],
-                        s[29], s[28], s[27], s[26], s[25], s[24], s[23], s[22], s[21],
-                        s[20], s[19], s[18], s[17], s[16], s[15], s[14], s[13], s[12],
-                        s[11], s[10], s[9], s[8], s[7], s[6], s[5], s[4], s[3], s[2], 
-                        s[1], s[0], feedback};
+            states <= { s[56:0], feedback};
         end
     end
 endmodule
