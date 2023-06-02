f37f596175effc4a9b86331b554bd428f1436c80
ryan <rbride@mail.bradley.edu>
2023-06-02 17:33:21 -0400

    Added two ripple adder loop generate examples

--- Fun_Gen_Examples.sv
+++ Fun_Gen_Examples.sv
@@ -171,3 +171,94 @@ module mod_c;
     end
 endmodule
 
+// Example 2 from page 790, A parameterized gray-code-to-binary-code coverter module using a loop
+// to generate continous assignments
+module gray2bin1 (bin, gray);
+    parameter SIZE = 8;     //This module is parameterizable by changing the input param
+    output [SIZE-1:0] bin;
+    input [SIZE-1:0] gray;
+
+    genvar i; 
+    generate
+        for ( i=0; i<SIZE; i++) begin : bitnum
+            assign bin[i] = ^gray[SIZE-1:i];
+            // i refers to the implicityly defined localparam whose 
+            // value in each instance of the generate block is
+            // the value of the genvar when its elaborated 
+        end
+    endgenerate 
+endmodule
+
+// Paramterized modules of ripple addders using a loop to generate SV gate primitives. The example below
+// Uses a two-dimensional net declaration outsie of the genrate loop to make the connections between the gate primitives
+
+module ripple_adder_gen1 ( co, sum, a, b, ci );
+    parameter SIZE = 4;
+    output [SIZE-1:0] sum;
+    output            co;
+    input  [SIZE-1:0] a, b;
+    input             ci;
+    wire   [SIZE  :0] c;
+    wire   [SIZE-1:0] t [1:3];
+    
+    genvar i;
+
+    assign c[0] = ci
+
+    // Hierarchical gate instance names are:
+    // xor gates: bitnum[0].g1    bitnum[1].g1    bitnum[2].g1    bitnum[3].g1
+    //            bitnum[0].g2    bitnum[1].g2    bitnum[2].g2    bitnum[3].g2
+    // and gates: bitnum[0].g3    bitnum[1].g3    bitnum[2].g3    bitnum[3].g3    
+    //            bitnum[0].g4    bitnum[1].g4    bitnum[2].g4    bitnum[3].g4
+    // or gates:  bitnum[0].g5    bitnum[1].g5    bitnum[2].g5    bitnum[3].g5
+    // Generated instances are connected with the
+    // The multidimensional nets t[1][3:0]     t[2][3:0]     t[3][3:0]      (12 nets total)    
+
+    for ( i=0; i<SIZE; i++ ) begin : bitnum 
+        xor g1 ( t[1][i],   a[i],       b[i]    );
+        xor g2 ( sum[i],    t[1][i],    c[i]    );
+        and g3 ( t[2][i],   a[i],       b[i]    );
+        and g4 ( t[3][i],   t[1][i],    c[i]    );
+        or  g5 ( c[i+1],    t[2][i],    t[3][i] );
+    end
+
+    assign co = c[SIZE];
+
+endmodule    
+
+
+// Second parametrized module of a ripple adder which instead uses a net declared within the generate loop
+module addergen1 (co, sum, a, b, ci);
+    parameter SIZE = 4;
+    output [SIZE-1:0] sum;
+    output co;
+    input [SIZE-1:0] a, b;
+    input ci;
+    wire [SIZE :0] c;
+    
+    genvar i;
+    
+    assign c[0] = ci;
+        // Hierarchical gate instance names are:
+        // xor gates: bitnum[0].g1 bitnum[1].g1 bitnum[2].g1 bitnum[3].g1
+        // bitnum[0].g2 bitnum[1].g2 bitnum[2].g2 bitnum[3].g2
+        // and gates: bitnum[0].g3 bitnum[1].g3 bitnum[2].g3 bitnum[3].g3
+        // bitnum[0].g4 bitnum[1].g4 bitnum[2].g4 bitnum[3].g4
+        // or gates: bitnum[0].g5 bitnum[1].g5 bitnum[2].g5 bitnum[3].g5
+        // Gate instances are connected with nets named:
+        // bitnum[0].t1 bitnum[1].t1 bitnum[2].t1 bitnum[3].t1
+        // bitnum[0].t2 bitnum[1].t2 bitnum[2].t2 bitnum[3].t2
+        // bitnum[0].t3 bitnum[1].t3 bitnum[2].t3 bitnum[3].t3
+    for(i=0; i<SIZE; i=i+1) begin : bitnum
+        wire t1, t2, t3;
+        
+        xor g1 ( t1,        a[i],   b[i]);
+        xor g2 ( sum[i],    t1,     c[i]);
+        and g3 ( t2,        a[i],   b[i]);
+        and g4 ( t3,        t1,     c[i]);
+        or  g5 ( c[i+1],    t2,     t3);
+    
+    end
+    assign co = c[SIZE];
+
+endmodule 
