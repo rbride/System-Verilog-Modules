896f06aa5b803ef6f3f36af3fce029a5874a805e
ryan <rbride@mail.bradley.edu>
2023-06-09 14:58:31 -0400

    add shift portion to function

--- Parallel_Descrambler.sv
+++ Parallel_Descrambler.sv
@@ -12,8 +12,6 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-//Implements a LFSR next state. Shifts bits, Input data is xord with lfsr feedback 
-//Computes A set of bit masks, then uses bit masks to select bits for XORing to compute the next state
 module Descrambler_64bit(
     input CLK,
     input rst,
@@ -25,12 +23,15 @@ module Descrambler_64bit(
     wire [57:0] lfsr_state_out; //Output of those registers
 
     //Set State in of LFSR to State Out every posedge CLK
+    //...
+    //...
+
 
 endmodule
 
+
 //Set FEED_FORWARD to 0 (off) for scrambling, 1 for Descrambling
 //LFSR_WIDTH localparam because always g(x) = x^58 + x^39 + 1
-//Potentially add Param Option to reverse lsb to msb bits ordering
 module Parallel_LFSR #(
     parameter DATA_WIDTH = 64,
     parameter FEED_FORWARD = 0,  
@@ -51,6 +52,50 @@ function [LFSR_WIDTH+DATA_WIDTH-1:0] lfsr_mask(input [31:0] index);
     reg [LFSR_WIDTH-1:0] mask_output_state[DATA_WIDTH-1:0];
     reg [DATA_WIDTH-1:0] mask_output_data[DATA_WIDTH-1:0];
 
+    reg [LFSR_WIDTH-1:0] state_value;
+    reg [DATA_WIDTH-1:0] data_value;
+
+    reg [DATA_WIDTH-1:0] data_mask;
+
+    interger i, j; //Index for arrays
+
+    begin 
+        //initialize bit masks
+        for ( i=0; i<LFSR_WIDTH; i++ ) begin
+            lfsr_mask_state_in[i] = 0;
+            lfsr_mask_state_in[i][i] = 1'b1;
+            lfsr_mask_data[i] = 0;
+        end
+        for ( i=0; i<DATA_WIDTH; i++ ) begin
+            mask_output_state[i] = 0;
+            if ( i<LFSR_WIDTH ) begin
+                mask_output_state[i][i] = 1'b1;
+            end
+            output_mask_data[i] = 0;
+        end
+
+        //Shift register work
+        for ( data_mask = {1'b1, {DATA_WIDTH-1{1'b0}}}; data_mask != 0; data_mask = data_mask >> 1) begin
+            //Determine shif in value, if val in last FF xor with input data bit (Assumes MSB)
+            state_value = lfsr_mask_state_in[LFSR_WIDTH-1];
+            data_value = lfsr_mask_data[LFSR_WIDTH-1];
+            data_value = data_value ^ data_mask;
+
+            // LFSR_Poly = 58'h8000000001 1 at the end for the +1 8 is for + 38 the highest val ignored
+            localparam Polynom = 58'h8000000001;
+            //Add XOR inputs 
+            for ( j=1; j<LFSR_WIDTH; j++ ) begin
+                if (( Polynom >> j ) & 1 ) begin
+                    state_value = lfsr_mask_state_in[j-1] ^ state_value;
+                    data_value = lfsr_mask_data[j-1] ^ data_value;
+                end
+            end
+
+            //Shift 
+
+        end
+
+    end
 endfunction
 
 
