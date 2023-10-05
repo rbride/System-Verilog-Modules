d70cb1e2612b23b94111054257f97658576d7312
ryan <rbride@mail.bradley.edu>
2023-10-04 21:21:13 -0400

    idk

--- fmc_i2c_controller.sv
+++ fmc_i2c_controller.sv
@@ -1,4 +1,6 @@
+`resetall
 `timescale 1ns / 1ps
+`default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer: Ryan Bride 
 // Create Date: 09/04/2023 11:58:18 AM
@@ -9,21 +11,19 @@
 // Revision 1 - Initial Design  
 //////////////////////////////////////////////////////////////////////////////////
 
-module fmc_i2c_controller #(
-    //I2C addresses of Slave modules of FMC484
-    parameter SI5338B_ADR   = 7'b1110000,
+module fmc_i2c_controller(
+    input wire CLK,
+    output wire SCL,
+    output wire SDA
+);
+      //I2C addresses of Slave modules of FMC484
+
+localparam[6:0]
+    SI5338B_ADR   = 7'b1110000,
      //Wether you are talking to Module A or B is determ by selection bit of CLPD
-    parameter QSFP_MOD      = 7'b1010000    
+    QSFP_MOD      = 7'b1010000;    
     //CLPD has various
-    )(
-    
-    
+        
 endmodule
 
-module Parallel_LFSR #(
-    parameter DATA_WIDTH = 64,
-    parameter FEED_FORWARD = 0,  
-    localparam LFSR_WIDTH = 58,
-    // LFSR_Poly = 58'h8000000001 1 at the end for the +1 8 is for + 38 the highest val ignored
-    localparam Polynom = 58'h8000000001
-)(
+`resetall
