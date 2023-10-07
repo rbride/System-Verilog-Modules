09e265a83bff0a59ab168eeba9b1724b3bf7e9ce
ryan <rbride@mail.bradley.edu>
2023-10-06 20:51:19 -0400

    buffer added

--- fmc_i2c_controller.sv
+++ fmc_i2c_controller.sv
@@ -10,20 +10,35 @@
 //      Master I2C Controller that Connects to and controls FMC424 add on board
 // Revision 1 - Initial Design  
 //////////////////////////////////////////////////////////////////////////////////
-
+//for reference upon completion for hookup
+//https://docs.xilinx.com/r/en-US/ug571-ultrascale-selectio/SelectIO-Interface-Attributes-and-Constraints
 module fmc_i2c_controller(
     input wire CLK,
-    output wire SCL,
-    output wire SDA
+    input wire en_fmc_board,
+    inout wire SCL_PIN,
+    inout wire SDA_PIN
 );
-      //I2C addresses of Slave modules of FMC484
-
-localparam[6:0]
+    //3 Wires used for SCL and SDA
+    wire scl_in, scl_out, scl_t;
+    wire sda_in, sda_out, sda_t;
+    //Create I/O Buffer For SCL PIN
+    IOBUF scl_buf(
+        .O(scl_out),
+        .I(scl_in),
+        .IO(SCL_PIN),
+        .T(scl_t)
+    );
+    //Create I/O Buffer 
+      
+    //I2C addresses of Slave modules of FMC484
+    localparam[6:0]
     SI5338B_ADR   = 7'b1110000,
      //Wether you are talking to Module A or B is determ by selection bit of CLPD
     QSFP_MOD      = 7'b1010000;    
     //CLPD has various
+    
+    
+    
         
 endmodule
-
 `resetall
