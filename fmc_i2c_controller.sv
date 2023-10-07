2f8f1d0126f2b3fa5d5d175349a4c74c07cfdf2e
ryan <rbride@mail.bradley.edu>
2023-10-06 20:53:20 -0400

    added both buffers

--- fmc_i2c_controller.sv
+++ fmc_i2c_controller.sv
@@ -18,6 +18,7 @@ module fmc_i2c_controller(
     inout wire SCL_PIN,
     inout wire SDA_PIN
 );
+
     //3 Wires used for SCL and SDA
     wire scl_in, scl_out, scl_t;
     wire sda_in, sda_out, sda_t;
@@ -29,16 +30,20 @@ module fmc_i2c_controller(
         .T(scl_t)
     );
     //Create I/O Buffer 
-      
-    //I2C addresses of Slave modules of FMC484
-    localparam[6:0]
-    SI5338B_ADR   = 7'b1110000,
-     //Wether you are talking to Module A or B is determ by selection bit of CLPD
-    QSFP_MOD      = 7'b1010000;    
-    //CLPD has various
-    
-    
-    
-        
+    IOBUF sda_buf(
+        .O(sda_out),
+        .I(sda_in),
+        .IO(SDA_PIN),
+        .T(sda_t)
+    );
+
 endmodule
 `resetall
+
+    //     //I2C addresses of Slave modules of FMC484
+    // localparam[6:0]
+    // SI5338B_ADR   = 7'b1110000,
+    //  //Wether you are talking to Module A or B is determ by selection bit of CLPD
+    // QSFP_MOD      = 7'b1010000;    
+    // //CLPD has various
+    
